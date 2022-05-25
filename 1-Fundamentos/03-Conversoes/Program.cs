e81c7504c8de54f7f55074fe9ba10ffb250aa49e
sidneibrianti <scbrianti2022@gmail.com>
2022-05-25 18:03:32 -0300

    Adição mais metodos de conversão

--- 1-Fundamentos/03-Conversoes/Program.cs
+++ 1-Fundamentos/03-Conversoes/Program.cs
@@ -2,6 +2,9 @@
 Console.WriteLine("Conversões");
 
 ConversaoImplicita();
+ConversaoExplicita();
+ConversaoParse();
+ClasseConvert();
 
 void ConversaoImplicita()
 {
@@ -24,4 +27,44 @@ void ConversaoImplicita()
     Console.WriteLine(num4);
     Console.WriteLine(num6);
     Console.WriteLine(numero);
+}
+
+void ConversaoExplicita()
+{
+    ushort num1 = 500;
+    byte num2 = (byte)num1;
+
+    float num3 = 2500.786f;
+    int num4 = (int)1985.75f;
+
+    char letra = (char)97;
+    Console.WriteLine(num1);
+    Console.WriteLine(num2);
+    Console.WriteLine(num3);
+    Console.WriteLine(num4);
+    Console.WriteLine(letra);
+}
+
+void ConversaoParse()
+{
+    string txtNumero = "1985";
+    int numero = int.Parse(txtNumero);
+    byte num1 = byte.Parse("120");
+    double num2 = double.Parse("125623,57");
+    float num3 = float.Parse("457,75");
+    Console.WriteLine(txtNumero);
+    Console.WriteLine(numero);
+    Console.WriteLine(num1);
+    Console.WriteLine(num2);
+    Console.WriteLine(num3);
+}
+
+void ClasseConvert()
+{
+    string texto = Convert.ToString(2500);
+    double num1 = Convert.ToDouble(false);
+    int num2 = Convert.ToInt32('C');
+    Console.WriteLine(texto);
+    Console.WriteLine(num1);
+    Console.WriteLine(num2);
 }
