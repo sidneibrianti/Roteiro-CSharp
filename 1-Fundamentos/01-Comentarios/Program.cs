c134b01f1caa1993bd68cb5d41eec6c625c7104f
sidneibrianti <scbrianti2022@gmail.com>
2022-05-25 15:01:00 -0300

    Estudo Comentários

---
+++ 1-Fundamentos/01-Comentarios/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//  Comentários de Linhas
+
+Console.WriteLine("Comentários");
+
+/*  
+    Comentários de multiplas linhas.
+    Linha 2
+    .
+    .
+    .
+    .
+    Linha n
+*/
+
+/// <summary>
+/// The minha classe.
+/// </summary>
+
+public class MinhaClasse {
+    //TODO:
+    }
