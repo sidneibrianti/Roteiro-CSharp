dbf6b35ecb180872f1e35c2eff99e5c08e4f3e8a
sidneibrianti <scbrianti2022@gmail.com>
2022-06-10 19:43:44 -0300

    Estudo Repetições

---
+++ 1-Fundamentos/10-Repeticao/Program.cs
@@ -0,0 +1,52 @@
+﻿
+Console.Clear();
+Console.WriteLine("Estruturas de Repetição");
+Console.WriteLine();
+For();
+While();
+DoWhile();
+
+void For()
+{
+    for (int i = 0; i <= 10; i++)
+    {
+        Console.WriteLine(i);
+    }
+
+    Console.WriteLine();
+
+    for (int j = 10; j >= 0; j--)
+    {
+        Console.WriteLine(j);
+    }
+
+    Console.WriteLine();
+    string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+    foreach (string i in cars)
+    {
+        Console.WriteLine(i);
+    }
+
+}
+
+void While()
+{
+    int i = 0;
+    while (i <= 10)
+    {
+        Console.WriteLine(i);
+        i++;
+    }
+    Console.WriteLine();
+}
+
+void DoWhile()
+{
+    int i = 0;
+    do
+    {
+        Console.WriteLine(i);
+        i++;
+    }
+    while (i < 10);
+}
